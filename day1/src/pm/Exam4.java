1fc47d00aad774556af7919bf2585a5fa4c70a46
Austin <austiny@snu.ac.kr>
2022-03-02 13:39:33 +0900

    aa

--- day1/src/pm/Exam4.java
+++ day1/src/pm/Exam4.java
@@ -5,6 +5,14 @@ public class Exam4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 자료형
+		// 변수 선언과 함께 값 대입
+		int a = 3;
+		
+		int b;
+		b = 4;
+		
+		System.out.println(a);
+		System.out.println(b);
 	}
 
 }
