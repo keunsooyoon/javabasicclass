08c8c594b9c00549fa6203b28a04c6a1f837f3a2
Austin <austiny@snu.ac.kr>
2022-03-16 14:09:55 +0900

    5

---
+++ day10/src/am/Sample.java
@@ -0,0 +1,94 @@
+package am;
+
+
+interface Predator{
+	String getFood();
+	
+	default void printFood() {
+		System.out.printf("my food is %s\n", getFood());
+	}
+	
+	int LEG_COUNT = 4;
+	
+	static int speed() {
+		return LEG_COUNT * 30;
+	}
+}
+
+
+class Animal {
+	String name;
+	
+	void setName(String name) {
+		this.name = name;
+		
+	}
+}
+
+class Tiger extends Animal implements Predator{
+	public String getFood() {
+		return "apple";
+	}
+}
+
+class Lion extends Animal implements Predator {
+	public String getFood() {
+		return "banana";
+	}
+}
+
+class Crocodile extends Animal implements Predator {
+	public String getFood() {
+		return "strawberry";
+	}
+}
+
+class Leopard extends Animal implements Predator {
+	public String getFood() {
+		return "orange";
+	}
+}
+
+
+
+class ZooKeeper {
+	void feed(Predator predator) {
+		System.out.println("feed " + predator.getFood());
+	}
+//	void feed(Predator tiger) {
+//		System.out.println("feed apple");
+//	}
+//	void feed(Predator lion) {
+//		System.out.println("feed banana");
+//	}
+//	
+//	void feed(Predator crocodile) {
+//		System.out.println("feed strawberry");
+//	}
+//	void feed(Predator leopard) {
+//		System.out.println("feed orange");
+//	}
+}
+
+class Bouncer {
+	void barkAnimal(Animal animal) {
+		if(animal instanceof Tiger) {
+			System.out.println("어흥");
+		} else if (animal instanceof Lion) {
+			System.out.println("으르렁");
+		}
+	}
+}
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		ZooKeeper zooKeeper = new ZooKeeper();
+		Tiger tiger = new Tiger(); 
+		Lion lion = new Lion();
+		zooKeeper.feed(tiger);//매서드 오버로딩
+		zooKeeper.feed(lion);
+	}
+
+}
